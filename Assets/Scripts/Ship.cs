b61abb413805c8053cba860fdbb4e5158f13866a
Giancarlo Sanchez <g.sanchez@glofox.com>
2016-12-12 17:10:17 +0000

    Camera configuration when turning a little more

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -43,8 +43,8 @@ public class Ship : MonoBehaviour {
 		
 		Vector3 destin = marker.position - transform.position;
 		_rb.AddForce(destin *10f,ForceMode.Acceleration);
-		float angleY = Mathf.LerpAngle(transform.eulerAngles.y, marker.position.x*1f, Time.time);
-		float angleX = Mathf.LerpAngle(transform.eulerAngles.x, -marker.position.y*2f, Time.time);
+		float angleY = Mathf.LerpAngle(transform.eulerAngles.y, marker.position.x*4f, Time.time);
+		float angleX = Mathf.LerpAngle(transform.eulerAngles.x, -marker.position.y*3.5f, Time.time);
 		transform.eulerAngles = new Vector3 (angleX, angleY, -4 * _rb.velocity.x);
 	}
 
