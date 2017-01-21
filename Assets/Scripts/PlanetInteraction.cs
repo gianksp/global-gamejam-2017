531e37a1eedcd8d2ea67fdab7da47d5dcf31d90c
djbyrne <donaljbyrne@me.com>
2017-01-21 19:44:49 +0000

    shockwave

---
+++ Assets/Scripts/PlanetInteraction.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlanetInteraction : MonoBehaviour {
+
+	public GameObject explosion;
+
+	public float gazeTime = 2f;
+	private float timer;
+
+	private bool gazedAt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gazedAt) 
+		{
+			
+			timer += Time.deltaTime;
+			Debug.Log (timer);
+
+			if (timer >= gazeTime) 
+			{
+				ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current),ExecuteEvents.pointerDownHandler);
+				timer = 0f;
+
+			}
+		}
+		
+	}
+
+	public void pointerEnter()
+	{
+		//Debug.Log ("pointer enter");
+		gazedAt = true;
+	}
+
+	public void pointerExit()
+	{
+		//Debug.Log ("pointer exit");
+		gazedAt = true;
+	}
+
+	public void pointerDown()
+	{
+		Debug.Log ("pointer down");
+		//Instantiate (explosion,new Vector3 (3.85f, 0.9f, 3.51f),Quaternion.identity);
+		Destroy (this.gameObject);
+	}
+
+}
---
+++ Assets/Scripts/ShockwaveEmitter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveEmitter : MonoBehaviour {
+
+	public float rotMin = -90;
+	public float rotMax = 90;
+
+	public float scaleMin;
+	public float scaleMax;
+
+	//color
+
+	public float timeMin;
+	public float timeMax;
+
+	private float timer;
+	public float interval = 5;
+
+	public GameObject prefab;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		if (timer >= interval) {
+			deploy ();
+			timer = 0;
+		}
+
+		
+	}
+
+	public void deploy()
+	{
+
+		//interval
+		timer = Random.Range(3f,10f);
+		//rotation
+		float rotZ = Random.Range (rotMin, rotMax);
+
+
+		//scale
+		float scaleRate = Random.Range(scaleMin,scaleMax);
+
+		//timer
+		float interval = Random.Range(timeMin,timeMax);
+
+		prefab.GetComponent<Shockwave>().scaleRate = scaleRate;
+
+		Quaternion rotation = Quaternion.Euler(0, 0, rotZ);
+
+		Instantiate(prefab,new Vector3(-2.011646f,-0.8410382f,21.08f),rotation);
+
+	}
+}
