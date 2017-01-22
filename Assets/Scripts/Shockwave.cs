d21ab0925064560d6948dafd8ce5ff6023ca6d99
Giancarlo Sanchez <g.sanchez@glofox.com>
2017-01-22 07:07:27 +0000

    Boss

--- Assets/Scripts/Shockwave.cs
+++ Assets/Scripts/Shockwave.cs
@@ -12,15 +12,16 @@ public class Shockwave : MonoBehaviour {
 	public float points = 5f;
 
 	public GameObject explosion;
-	private float scale = 1;
+	public float scale = 1f;
 
 	// Use this for initialization
 	void Start () {
+		GameObject.Instantiate (explosion, transform.position, transform.rotation);
 		Invoke ("Die", Random.Range(4,6));
 		speedRate = Random.Range (1, 4);
 		limitZ = GameObject.FindGameObjectWithTag("Player").transform.position.z;
-		transform.position = new Vector3 (Random.Range (-20, 20), Random.Range(-5,5), Random.Range(100,300));
-		transform.Rotate (new Vector3 (Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360)));
+		transform.position = new Vector3 (Random.Range (-30, 30), Random.Range(-15,15), Random.Range(100,300));
+		transform.Rotate (new Vector3 (Random.Range (-15, 15), 0, Random.Range (-180, 180)));
 //		_rb = gameObject.GetComponent<Rigidbody> ();
 	}
 
@@ -29,16 +30,19 @@ public class Shockwave : MonoBehaviour {
 		scale += 0.1f;
 		transform.localScale = new Vector3(transform.localScale.x+scale,0.01f,transform.localScale.x+scale);
 //		transform.Translate(-Vector3.forward*speedRate);
-//		if (limitZ - transform.position.z >= 10) {
-//			Destroy (gameObject);
-//		}
+		if (scale >= 11f) {
+			Destroy (gameObject);
+		}
 	}
 
 	void OnCollisionEnter(Collision collision) {
 
-		GameObject.Instantiate (explosion, collision.transform.position, collision.transform.rotation);
-		Debug.Log (collision.transform.tag+" destroyed");
-		Destroy (collision.gameObject);
+		if (collision.transform.tag == "Obstacle" || collision.transform.tag == "Ship") {
+			Debug.Log (collision.transform.tag + " destroyed");
+
+			GameObject.Instantiate (explosion, collision.transform.position, collision.transform.rotation);
+			Destroy (collision.gameObject);
+		}
 //		if (collision.transform.tag == "Bullet") {
 //			Bullet bullet = collision.transform.gameObject.GetComponent<Bullet>();
 //			hp = hp - bullet.damage;
--- Assets/Scripts/ShockwaveController.cs
+++ Assets/Scripts/ShockwaveController.cs
@@ -7,7 +7,7 @@ public class ShockwaveController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating("CreateShockwave", 0f, 4f);
+		InvokeRepeating("CreateShockwave", 0f, 10f);
 	}
 
 	// Update is called once per frame
